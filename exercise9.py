f2c0add60d7da8dafaad0f3fdc1d59addb7eb2e2
Foxyes <108577021+lilFoxyes@users.noreply.github.com>
2023-09-10 14:15:16 -0300

    Add files via upload

---
+++ exercise9.py
@@ -0,0 +1,2 @@
+n = int(input("Escreva um número e irei mostrar a tabuada de 10 dele: "))
+print("A tabuada de 10 do número {} é: \n 1x{} = {} \n 2x{} = {} \n 3x{} = {} \n 4x{} = {} \n 5x{} = {} \n 6x{} = {} \n 7x{} = {} \n 8x{} = {} \n 9x{} = {} \n 10x{} = {}".format(n, n, n*1, n, n*2, n, n*3, n, n*4, n, n*5, n, n*6, n, n*7, n, n*8, n, n*9, n, n*10 ))
